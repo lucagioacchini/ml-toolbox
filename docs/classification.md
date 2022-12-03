c0720d3b4578c1972fa14d19b61aec9afc8929fa
lucagioacchini <lucagioack@gmail.com>
2022-12-03 22:01:48 +0100

    example

--- docs/classification.md
+++ docs/classification.md
@@ -29,48 +29,40 @@ Description.
  ### **Methods** 
   
   
-
-  ```python
-  _init_model(io)
-
-  ```
-Description.
-
-  **Parameters** 
-   - **io**(_tuple_) - Description
-
-
-  **Returns** 
-
-
-<br><br>
-
-  ```python
-_scale_data(self, X_train, X_val)
-
-```
-Description.
-
-  **Parameters** 
-   - **io**(_tuple_) - Description
-
-
-  **Returns** 
-
-
-<br><br>
+ ### **Examples** 
 
 ```python
-_encode_labels(self, y_train, y_val)
+>>> # Generate 20 (resp. 5) training (resp. validation) samples with 4 features 
+>>> X_train, X_val = np.random.random((20, 4)), np.random.random((5, 4))
+>>> # Generate binary classes
+>>> y_train, y_val = np.random.randint(0,2, (20)), np.random.randint(0,2, (5))
+>>> X_train, y_train
 
+(array([[0.61798233, 0.65360835, 0.1029108 , 0.54929112],
+        [0.89883498, 0.53387149, 0.30059125, 0.26111361],
+        [0.23928837, 0.20361755, 0.35225478, 0.76946751], 
+        [     ...                               ...     ]),
+ array([0, 0, 0, ...]))
+
+>>> from mltoolbox.classification import DeepClassifier
+>>> from keras import layers # Define architecture
+>>> inputs = layers.Input((4,))
+>>> hidden = layers.Dense(64, activation='relu')(inputs)
+>>> outputs = layers.Dense(2, activation='softmax')(hidden)
+>>> # Initialize the classifier
+>>> classifier = DeepClassifier(io=(inputs, outputs))
+>>> # Train the classifier for 3 epochs. Standardize data before training
+>>> classifier.fit(training_data=(X_train, y_train), scale_data=True, epochs=3)
+
+Epoch 1/3
+1/1 [==============================] - 0s 482ms/step - loss: 0.6901 - accuracy: 0.7000
+Epoch 2/3
+1/1 [==============================] - 0s 4ms/step - loss: 0.6846 - accuracy: 0.7000
+Epoch 3/3
+1/1 [==============================] - 0s 4ms/step - loss: 0.6792 - accuracy: 0.7500
+
+# Predict the validation labels
+>>> classifier.predict(X_val)
+
+array([0, 0, 0, 0, 1])
 ```
-Description.
-
-  **Parameters** 
-   - **io**(_tuple_) - Description
-
-
-  **Returns** 
-  
-
-  <br><br>
--- mltoolbox/classification/deep_classifier.py
+++ mltoolbox/classification/deep_classifier.py
@@ -3,9 +3,21 @@ from ..preprocessing import OneHotLabelEncoder
 from keras.models import load_model, Model
 from keras.callbacks import ModelCheckpoint
 import numpy as np
+import joblib
 
 class DeepClassifier():
     def __init__(self, model_path=None, io=None, _load_model=False):
+        """_summary_
+
+        Parameters
+        ----------
+        model_path : _type_, optional
+            _description_, by default None
+        io : _type_, optional
+            _description_, by default None
+        _load_model : bool, optional
+            _description_, by default False
+        """
         self.model = None
         self.scaler = StandardScaler()
         self.label_encoder = OneHotLabelEncoder()
@@ -23,6 +35,18 @@ class DeepClassifier():
 
             
     def _init_model(self, io):
+        """_summary_
+
+        Parameters
+        ----------
+        io : _type_
+            _description_
+
+        Returns
+        -------
+        _type_
+            _description_
+        """
         classifier = Model(io[0], io[1])
         classifier.compile(optimizer='adam', loss='categorical_crossentropy', 
                            metrics=['accuracy'])
@@ -30,6 +54,20 @@ class DeepClassifier():
         return classifier
     
     def _scale_data(self, X_train, X_val):
+        """_summary_
+
+        Parameters
+        ----------
+        X_train : _type_
+            _description_
+        X_val : _type_
+            _description_
+
+        Returns
+        -------
+        _type_
+            _description_
+        """
         # Fit the scaler on training data
         self.scaler.fit(X_train)
         # Scale training data
@@ -42,6 +80,20 @@ class DeepClassifier():
     
     
     def _encode_labels(self, y_train, y_val):
+        """_summary_
+
+        Parameters
+        ----------
+        y_train : _type_
+            _description_
+        y_val : _type_
+            _description_
+
+        Returns
+        -------
+        _type_
+            _description_
+        """
         # Fit the encoder on training labels
         self.label_encoder.fit(y_train) 
         # Encode training labels
@@ -55,6 +107,30 @@ class DeepClassifier():
     
     def fit(self, training_data, validation_data = None, scale_data=True, 
             epochs=10, batch_size=256, verbose=1, save=False):
+        """_summary_
+
+        Parameters
+        ----------
+        training_data : _type_
+            _description_
+        validation_data : _type_, optional
+            _description_, by default None
+        scale_data : bool, optional
+            _description_, by default True
+        epochs : int, optional
+            _description_, by default 10
+        batch_size : int, optional
+            _description_, by default 256
+        verbose : int, optional
+            _description_, by default 1
+        save : bool, optional
+            _description_, by default False
+
+        Returns
+        -------
+        _type_
+            _description_
+        """
         # Get training datasets
         X_train, y_train = training_data
         # If provided, get validation datasets
@@ -93,6 +169,20 @@ class DeepClassifier():
 
     
     def predict(self, X, scale_data=True):
+        """_summary_
+
+        Parameters
+        ----------
+        X : _type_
+            _description_
+        scale_data : bool, optional
+            _description_, by default True
+
+        Returns
+        -------
+        _type_
+            _description_
+        """
         y_pred_proba = self.predict_proba(X, scale_data)
         y_pred = np.argmax(y_pred_proba, axis=1)
         y_pred = self.label_encoder.inverse_transform(y_pred)
@@ -101,6 +191,20 @@ class DeepClassifier():
 
     
     def predict_proba(self, X, scale_data=True):
+        """_summary_
+
+        Parameters
+        ----------
+        X : _type_
+            _description_
+        scale_data : bool, optional
+            _description_, by default True
+
+        Returns
+        -------
+        _type_
+            _description_
+        """
         if scale_data:
             X = self.scaler.transform(X)
         return self.model(X).numpy()
