d75c4b25a3447de6ff851a4ae5ee951cd4f0ce56
Luca Gioacchini <38500957+lucagioacchini@users.noreply.github.com>
2022-12-03 20:16:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,5 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='mltoolbox',
-    version='0.0.1',
-    author='Luca Gioacchini',
-    author_email='luca.gioacchini@polito.it',
-    description='Testing installation of Package',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/lucagioacchini/MLToolbox',
     packages=['mltoolbox']
 )
