0084d9fe61b654dde95e18318cdff49e53bef22c
lucagioacchini <lucagioack@gmail.com>
2022-12-03 21:04:38 +0100

    ,

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
+    version='0.0.1',
     name='mltoolbox',
     packages=['mltoolbox'],
     install_requires=['requests']
