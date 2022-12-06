8fc99fc61922d332881f3e5d1ecd92a976acb3ba
lucagioacchini <lucagioack@gmail.com>
2022-12-06 15:19:25 +0100

    mae added

--- docs/representation.md
+++ docs/representation.md
@@ -70,3 +70,78 @@ ipsum   0.088425  0.077777 -0.096294
 
 (29, 10)
 ```
+
+
+## Multi-modal Autoencoder
+___
+```python
+class mltoolbox.representation.MultimodalAE(model_path=None, io=None, _load_model=False, losses=None, weights=None)
+
+```
+[`source code`](./../mltoolbox/representation/mae.py)
+
+Description.
+
+ ###  **Attributes** 
+   - **model_path**(_str_) - Description
+   - **io**(_tuple_) - Description
+   - **_load_model**(_bool_) - Description
+
+ ### **Methods** 
+  
+  
+ ### **Examples** 
+
+```python
+>>> # Define a two modalities dataset. The first modality with 15 features and
+>>> # The second with 20 ones
+>>> X1_train, X2_train = np.random.random((20, 15)), np.random.random((20, 20))
+>>> X_train = np.hstack([X1_train, X2_train]) # Stack the modalities
+>>> X_train.shape
+
+(20, 35)
+
+>>> # Define the architecture
+>>> inputs = layers.Input((35,))
+>>> # Encoder branch of modality 1
+>>> hidden1 = layers.Lambda(lambda x: x[:, :15])(inputs)
+>>> hidden1 = layers.Dense(32, activation='relu')(hidden1)
+>>> # Encoder branch of modality 2
+>>> hidden2 = layers.Lambda(lambda x: x[:, 15:20])(inputs)
+>>> hidden2 = layers.Dense(32, activation='relu')(hidden2)
+>>> # Concatenate
+>>> hidden = layers.Concatenate()([hidden1, hidden2])
+>>> # Common architecture
+>>> hidden = layers.Dense(32, activation='relu')(hidden)
+>>> # Bottleneck
+>>> hidden = layers.Dense(4, activation='relu', name='Coded')(hidden)
+>>> hidden = layers.Dense(32, activation='relu')(hidden)
+>>> hidden = layers.Dense(32*2, activation='relu')(hidden)
+>>> # Decoder branch of modality 1
+>>> hidden1 = layers.Dense(32, activation='relu')(hidden)
+>>> output1 = layers.Dense(15, activation='linear', name='out1')(hidden1)
+>>> # Decoder branch of modality 2
+>>> hidden2 = layers.Dense(32, activation='relu')(hidden)
+>>> output2 = layers.Dense(20, activation='linear', name='out2')(hidden2)
+>>> outputs = [output1, output2]
+>>> 
+>>> loss = {'out1':'mse', 'out2':'mse'} # Mean Squared Errors
+>>> weights = {'out1':15/35, 'out2':20/35} # Balance losses
+>>> 
+>>> mae = MultimodalAE(io=(inputs, outputs), losses=loss, weights=weights)
+>>> # Fit the multi-modal autoencoder
+>>> mae.fit(training_data=(X_train, X_train), y_sizes=[15, 20], epochs=3)
+
+Epoch 1/3
+1/1 [==============================] - 1s 703ms/step - loss: 0.3286 - out1_loss: 0.3294 - out2_loss: 0.3279
+Epoch 2/3
+1/1 [==============================] - 0s 4ms/step - loss: 0.3236 - out1_loss: 0.3252 - out2_loss: 0.3225
+Epoch 3/3
+1/1 [==============================] - 0s 4ms/step - loss: 0.3192 - out1_loss: 0.3213 - out2_loss: 0.3176
+
+>>> # Encoder the new features
+>>> embeddings = mae.transform(X_train)
+>>> X_train.shape, embeddings.shape
+
+((20, 35), (20, 4))
+```
--- mltoolbox/representation/mae.py
+++ mltoolbox/representation/mae.py
@@ -1,32 +1,33 @@
 # type: ignore
 
 from sklearn.preprocessing import StandardScaler
-from ..preprocessing import OneHotLabelEncoder
 from keras.models import load_model, Model
 from keras.callbacks import ModelCheckpoint
 import numpy as np
 import joblib
 
 class MultimodalAE():
-    def __init__(self, model_path=None, io=None, _load_model=False):
+    def __init__(self, model_path=None, io=None, _load_model=False, losses=None, 
+                 weights=None):
         self.model_path=model_path
         self.model = None
         self.scaler = StandardScaler()
         
         # If input and output layers are provided, init the classifier
         if type(io)==tuple:
-            self.model=self._init_model(io)
+            self.model = Model(io[0], io[1])
+            self.model.compile(optimizer='adam', loss=losses, 
+                               loss_weights=weights)
             
         if _load_model:
-            self.model = load_model(f'{self.model_path}_mae.h5')
-            self.scaler = joblib.load(f'{self.model_path}_scaler.save')
+            self._load_model()
 
-            
-    def _init_model(self, io):
-        mae = Model(io[0], io[1])
-        mae.compile(optimizer='adam', loss='mse')
-        
-        return mae
+    def _load_model(self):
+        self.model = load_model(f'{self.model_path}_mae.h5')
+        i,o = self.extract_encoder()
+        self.encoder = Model(i,o)
+        self.scaler = joblib.load(f'{self.model_path}_scaler.save')
+
     
     def _scale_data(self, X_train, X_val):
         # Scale training data
@@ -37,14 +38,27 @@ class MultimodalAE():
             
         return X_train, X_val
 
+    def _extract_y(self, y, y_size):
+        _y = []
+        for i in range(len(y_size)):
+            if i == 0:
+                _y.append(y[:, :y_size[i]])
+            elif i == len(y_size)-1:
+                _y.append(y[:, y_size[i-1]:])
+            else:
+                _y.append(y[:, y_size[i-1]:y_size[i]])
+
+        return _y
     
-    def fit(self, training_data, validation_data=None, scale_data=True, 
-            epochs=10, batch_size=256, verbose=1, save=False):
+    def fit(self, training_data, validation_data=None, y_sizes=[], verbose=1,
+            scale_data=True, epochs=10, batch_size=256, save=False):
         # Get training datasets
         X_train, y_train = training_data
+        y_train = self._extract_y(y_train, y_sizes)
         # If provided, get validation datasets
         if type(validation_data)==tuple:
             X_val, y_val = validation_data
+            y_val = self._extract_y(y_val, y_sizes)
         else:
             X_val, y_val = None, None
         
@@ -63,27 +77,41 @@ class MultimodalAE():
         if save:
             # Save the best model according to the max val_accuracy
             saver = ModelCheckpoint(filepath=f'{self.model_path}_mae.h5',
-                                    monitor='val_accuracy', mode='max', 
+                                    monitor='val_loss', mode='min', 
                                     save_best_only=True, verbose=False)
 
             history = self.model.fit(X_train, y_train, epochs=epochs, 
                                 validation_data=validation_data,
                                 batch_size=batch_size, shuffle=True, 
-                                sample_weight=self.label_encoder.weights, 
                                 callbacks=[saver], verbose=verbose) 
             joblib.dump(self.scaler, f'{self.model_path}_scaler.save')
-            joblib.dump(self.label_encoder, f'{self.model_path}_ohle.save')
+
+            self._load_model()
         else:
             history = self.model.fit(X_train, y_train, epochs=epochs, 
                                 validation_data=validation_data,
                                 batch_size=batch_size, shuffle=True, 
-                                sample_weight=self.label_encoder.weights, 
                                 verbose=verbose) 
+        i,o = self.extract_encoder()
+        self.encoder = Model(i,o)
         
         return history
+
+
+    def extract_encoder(self, trainable=False):
+        for n in range(len(self.model.layers)):
+            layer = self.model.layers[n]
+
+            if trainable: layer.trainable = False
+            else: layer.trainable = True
+
+            if n == 0: inputs = layer.input
+            elif layer.name == 'Coded': outputs = layer.output
+        
+        return inputs, outputs
 
     
     def transform(self, X, scale_data=True):
         if scale_data:
             X = self.scaler.transform(X)
-        return self.model(X).numpy()
+        return self.encoder(X).numpy()
